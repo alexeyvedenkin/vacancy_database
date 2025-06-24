8d3aa3e4fd7f68f454fc8db3c2bbfee4711874eb
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-24 16:41:17 +0500

    fixes src/employers.py

--- src/employers.py
+++ src/employers.py
@@ -1,4 +1,4 @@
-# from typing import Any
+from api_service import HeadHunterAPI
 
 
 class Employers():
@@ -7,15 +7,11 @@ class Employers():
     def __init__(self, data: dict) -> None:
         self.__employer_id: int = data.get('employer_id')
         self.__name: str = data.get('name')
-        # self.__town: str = data.get('area', {}).get('name', '')
         self.__open_vacancies: int = data.get('open_vacancies')
 
     def __str__(self) -> str:
         """ Возвращает формат для вывода строкового значения работодателя """
         return (f"{self.__name.strip()}, код {self.__employer_id}, количество вакансий: {self.__open_vacancies}")
-                # f"{self.__alternate_url.strip()}\n"
-                # f"Описание вакансии: {self.__description.strip():<100}\n"
-                # f"Требования к вакансии: {self.__requirement.strip():<100}\n\n")
 
     @property
     def name(self) -> str:
@@ -34,8 +30,24 @@ class Employers():
         employers_list = []
         for data in data_list:
             if isinstance(data, dict):
-                employers_id = data.get('employers_id')
-                if employers_id not in unique_employers:
-                    unique_employers[employers_id] = cls(data)
-                    employers_list.append(unique_employers[employers_id])
+                employer_id = data.get('employer_id')
+                if employer_id not in unique_employers:
+                    unique_employers[employer_id] = cls(data)
+                    employers_list.append(unique_employers[employer_id])
         return employers_list
+
+
+if __name__ == '__main__':
+    hh_api = HeadHunterAPI('data/vacancies.json')
+    query = hh_api.load_employers('УРАЛСИБ')
+
+    # Check if any employers were found to avoid IndexError
+    if query:
+        print(f"Number of employers found: {len(query)}")
+        print("Query data:", query[0])  # Access the first element only if the list is not empty
+        employers = Employers.from_list(query)
+        print("Employers:")
+        for employer in employers:
+            print(employer)
+    else:
+        print("No employers found.")
