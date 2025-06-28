e600eb4b494872efdd0d0dd55aef7013d45b2047
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-29 03:51:13 +0500

    fixed config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 ROOT_DIR = os.path.dirname(__file__)
 LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+EMPLOYERS_ID = [4934, 89, 15478, 107434, 1808, 909495, 1669269, 20189, 3809, 1305791]
