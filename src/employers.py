a0f45f3fb578988a2b2352dca5e954ff30a7f9f2
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-06-29 04:00:19 +0500

    fixed src/employers.py

--- src/employers.py
+++ src/employers.py
@@ -1,27 +1,15 @@
-from api_service import HeadHunterAPI
-
-
 class Employers():
     """ Определяет параметры для подбора работодателей с API-сервиса api.hh.ru """
 
     def __init__(self, data: dict) -> None:
-        self.__employer_id: int = data.get('employer_id')
-        self.__name: str = data.get('name')
-        self.__open_vacancies: int = data.get('open_vacancies')
+        self.employer_id: int = data.get('employer_id')
+        self.name: str = data.get('name')
+        self.open_vacancies: int = data.get('open_vacancies')
 
     def __str__(self) -> str:
         """ Возвращает формат для вывода строкового значения работодателя """
-        return (f"{self.__name.strip()}, код {self.__employer_id}, количество вакансий: {self.__open_vacancies}")
-
-    @property
-    def name(self) -> str:
-        """ Разрешает доступ к атрибуту name """
-        return self.__name
-
-    @property
-    def open_vacancies(self) -> int:
-        """ Разрешает доступ к атрибуту open_vacancies """
-        return self.__open_vacancies
+        name = self.name.strip() if self.name else "Без названия"
+        return (f"{self.employer_id}, {name}, количество вакансий: {self.open_vacancies}")
 
     @classmethod
     def from_list(cls, data_list: list) -> list:
@@ -35,19 +23,3 @@ class Employers():
                     unique_employers[employer_id] = cls(data)
                     employers_list.append(unique_employers[employer_id])
         return employers_list
-
-
-if __name__ == '__main__':
-    hh_api = HeadHunterAPI('data/vacancies.json')
-    query = hh_api.load_employers('УРАЛСИБ')
-
-    # Check if any employers were found to avoid IndexError
-    if query:
-        print(f"Number of employers found: {len(query)}")
-        print("Query data:", query[0])  # Access the first element only if the list is not empty
-        employers = Employers.from_list(query)
-        print("Employers:")
-        for employer in employers:
-            print(employer)
-    else:
-        print("No employers found.")
